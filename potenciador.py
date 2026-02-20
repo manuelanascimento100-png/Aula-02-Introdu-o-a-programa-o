cbfaff80479869b1d9d7c3487fb6cd1bbeb43da4
manuelanascimento100-png <manuela.nascimento100@al.unieduk.com.br>
2026-02-19 22:33:09 -0300

    Add files via upload

---
+++ potenciador.py
@@ -0,0 +1,14 @@
+#somador 
+print("#############################")
+print("           SOMADOR           ")
+print("#############################")
+
+a = input("entre com o primeiro número e tecle enter")
+b = input("entre com o segundo número e tecle enter")
+
+a = int(a)
+b = int(b)
+
+print(a ** b)
+
+input() # apenas para esperar o enter antes de fechar a janela 
