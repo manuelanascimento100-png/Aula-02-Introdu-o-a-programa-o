c9a4bdc0b6c0771e44fe5de4ac8d5faf33eff14a
manuelanascimento100-png <manuela.nascimento100@al.unieduk.com.br>
2026-02-26 19:52:37 -0300

    Add files via upload

---
+++ #raiz quadrada.py
@@ -0,0 +1,13 @@
+#raiz quadrada
+
+print ("##############")
+print ("   raiz quadrada   ")
+print ("##############")
+
+a = input ("entre com o primeiro numero e tecle enter")
+a = int(a)
+
+import math
+print (math.sqrt(a))
+
+input()
